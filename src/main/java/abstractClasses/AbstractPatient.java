c68a2b415e830217c320a5086b1f801600f808f4
sorin <sorynel255@yahoo.com>
2019-12-08 15:39:35 +0200

    Clasa abstracta 'AbstractPatient'

--- src/main/java/abstractClasses/AbstractPatient.java
+++ src/main/java/abstractClasses/AbstractPatient.java
@@ -1,4 +1,21 @@
 package abstractClasses;
 
-public class AbstractPatient {
+public class AbstractPatient{
+    private int patientID;
+    private String patient;
+
+    public AbstractPatient(int patientID, String patient){
+        this.patient = patient;
+        this.patientID = patientID;
+    }
+
+    public int getPatientID() {
+        return patientID;
+    }
+
+    public String getPatient() {
+        return patient;
+    }
+
+
 }
