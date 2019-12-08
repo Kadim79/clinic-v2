dc72454eb24e550b483702609e5aa8fd6f8993a8
Kadim79 <cdimu@icloud.com>
2019-12-08 15:35:45 +0200

    added initial files for the project

--- src/main/java/ClinicFileReader.java
+++ src/main/java/ClinicFileReader.java
@@ -1,3 +1,41 @@
-public class ClinicFileReader {
+import abstractClasses.AbstractPatient;
+import abstractClasses.ClinicReader;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+public class ClinicFileReader implements ClinicReader {
+
+
+    public List<AbstractPatient> readPatients() throws IOException, URISyntaxException {
+
+        readFileByName("human_patients.txt");
+
+        return null;
+    }
+
+    public Map<Integer, String> readProblems() throws IOException, URISyntaxException {
+
+        readFileByName("human_problems.txt");
+
+
+        return null;
+    }
+
+    private List<String>readFileByName(String fileName) throws IOException,URISyntaxException {
+
+        URI patients = ClassLoader.getSystemResource(fileName).toURI();
+        Path pathOfPatients = Paths.get(patients);
+        List<String> strings = Files.readAllLines(pathOfPatients);
+
+        return strings;
+
+    }
 }
 
