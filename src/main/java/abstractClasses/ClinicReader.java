161d93a4f8c4d95223aded6784f48e0c0041be66
sorin <sorynel255@yahoo.com>
2019-12-08 15:05:42 +0200

    MOTEDA INTERFATA

--- src/main/java/abstractClasses/ClinicReader.java
+++ src/main/java/abstractClasses/ClinicReader.java
@@ -1,4 +1,11 @@
 package abstractClasses;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
+
 public interface ClinicReader {
+    List<AbstractPatient> readPatients() throws IOException, URISyntaxException;
+    Map<Integer, String> readProblems() throws  IOException, URISyntaxException;
 }
