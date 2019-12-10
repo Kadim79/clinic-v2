2c23b654b2b9b50baf9bdd9c4fc2aacb228ff317
sorynel24 <sorynel255yahoo.com>
2019-12-10 20:15:23 +0200

    Clasa abstracta 'AbstractPatient' -- new Version

--- src/main/java/abstractClasses/AbstractClinic.java
+++ src/main/java/abstractClasses/AbstractClinic.java
@@ -1,9 +1,13 @@
 package abstractClasses;
 
-public class AbstractClinic {
+import java.util.Map;
+import java.util.List;
+
+public abstract class AbstractClinic {
 
     private int patientId;
     private String patientName;
+    Map<Integer, AbstractPatient> currentPatients;
 
     public AbstractClinic(int patientId, String patientName) {
         this.patientId = patientId;
@@ -17,5 +21,12 @@ public class AbstractClinic {
     public String getPatientName() {
         return patientName;
     }
+
+    public abstract void addPatient(AbstractPatient patient);
+    public abstract void addBulkPatient(List patients);
+    public abstract void removePatientByPatientObject(AbstractPatient patient);
+    public abstract void removeByPatientID(Integer patientId);
+    public abstract void listPatients();
+
 }
 
--- src/main/java/abstractClasses/AbstractPatient.java
+++ src/main/java/abstractClasses/AbstractPatient.java
@@ -1,11 +1,38 @@
 package abstractClasses;
 
+import java.util.List;
+
 public class AbstractPatient extends AbstractClinic {
 
     public AbstractPatient(int patientId, String patientName) {
         super(patientId, patientName);
         System.out.println(super.getPatientName()+" is now a patient in our clinic and has the following Id "
                            +super.getPatientId());
+    }
+
+    @Override
+    public void addPatient(AbstractPatient patient) {
+
+    }
+
+    @Override
+    public void addBulkPatient(List patients) {
+
+    }
+
+    @Override
+    public void removePatientByPatientObject(AbstractPatient patient) {
+
+    }
+
+    @Override
+    public void removeByPatientID(Integer patientId) {
+
+    }
+
+    @Override
+    public void listPatients() {
+
     }
 
     @Override
